d90b8303916e9aeb7760ddcc46ba4d2caff9ced2
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-30 04:45:51 -0700

    Add Interface and Map Classes

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
+/**** DOCUMENT *****/
+document.addEventListener("DOMContentLoaded", function() {
+
+  const canvasEl = document.getElementsByTagName("canvas")[0];
+  const main = new Main(canvasEl);
+  main.start();
+  window.addEventListener("keydown", (e) => main.keyDown(e.keyCode), true);
+  window.addEventListener("keyup", (e) => main.keyUp(e.keyCode), true);
+  window.addEventListener("mousemove", main.mousemove.bind(main));
+  window.addEventListener("mousedown", main.mousedown.bind(main));
+  window.addEventListener("mouseup", main.mouseup.bind(main));
+  window.addEventListener("mousewheel", main.mousewheel.bind(main));
+  window.addEventListener("DOMMouseScroll", main.mousewheel.bind(main));
+});
+
 class Main {
   constructor(canvas) {
     this.canvas = canvas;
     this.width = this.canvas.getAttribute("width");
     this.height = this.canvas.getAttribute("height");
+    this.map = new Map();
+    this.interface = new Interface(this.map);
+    this.gameObjects = {
+      this.interface(),
+      this.map()
+    }
   }
 
-  start(canvasEl) {
-    const ctx = canvasEl.getContext("2d");
+  start() {
+    const ctx = this.canvas.getContext("2d");
     const animateCallback = () => {
       this.update();
 
-      ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+      ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       this.draw(ctx);
       requestAnimationFrame(animateCallback);
     };
@@ -25,21 +46,14 @@ class Main {
   }
 
   draw(ctx) {
-    for (var i = 0; i < this.gameObjects.length; i++) {
+    for (var i = this.gameObjects.length - 1; i >= 0; i--) {
       this.gameObjects[i].draw(ctx);
     }
   }
 
-  mousedown(pt) {
-    this.mouseDrag = true;
-  }
 
-  mousemove() {
+  _update(delta) {
 
-  }
-
-  mouseup(pt) {
-    this.mouseDrag = false;
   }
 }
 
