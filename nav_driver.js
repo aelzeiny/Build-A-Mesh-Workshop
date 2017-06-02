51e17e1bc16661d75dded23662297fa22d88c863
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-01 19:22:06 -0700

    NavDriver hookup to main class

--- nav_driver.js
+++ nav_driver.js
@@ -9,8 +9,24 @@ document.addEventListener("DOMContentLoaded", function() {
   };
   resize();
   window.addEventListener("resize", resize);
-  const main = new Main(canvasEl);
-  main.start();
+
+  const views = new Array(5);
+  for(let i=1;i<=views.length;i++) {
+    views[i-1] = document.getElementById("step-" + i);
+  }
+
+  const main = new Main(canvasEl, views);
+
+  // Control events
+  document.getElementById("percentWalls").addEventListener("change", e => main.wallsPercent(e.currentTarget.value));
+  document.getElementById("percentWalls").addEventListener("input", e => main.wallsPercent(e.currentTarget.value));
+  // Step Flow Events
+  document.getElementById("next-1").addEventListener("click", (e) => main.next(1));
+  document.getElementById("next-2").addEventListener("click", (e) => main.next(2));
+  document.getElementById("next-3").addEventListener("click", (e) => main.next(3));
+  document.getElementById("reset").addEventListener("click", (e) => main.next(-1));
+
+  // Mouse sensory events
   window.addEventListener("keydown", (e) => main.keyDown(e.keyCode), true);
   window.addEventListener("keyup", (e) => main.keyUp(e.keyCode), true);
   window.addEventListener("mousemove", main.mousemove.bind(main));
@@ -19,4 +35,5 @@ document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("mousewheel", main.mousewheel.bind(main));
   window.addEventListener("DOMMouseScroll", main.mousewheel.bind(main));
   window.addEventListener("resize", main.setSize.bind(main));
+  main.start();
 });
